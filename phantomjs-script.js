997d91ca64e2992d9f33ba79bdae56bc099acc9d
aberridg <30839885+aberridg@users.noreply.github.com>
2017-09-13 19:00:25 +0100

    Update phantomjs-script.js

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,5 +1,47 @@
-console.log('start phantom');
-setTimeout(function() {
-    console.log('Boo from phantom');
-    phantom.exit();
-}, 1000);
+var page = require('webpage').create();
+page.open('https://andrewspc.co.uk:444/spotfire/wp/OpenAnalysis?file=de2c4800-5553-4062-8353-6047dd1d6eeb', function(status) {
+  console.log("Status: " + status);
+  if(status === "success") {
+  	console.log("success");
+  	//page.render("test212.png");
+  }
+  //phantom.exit();
+});
+
+page.onLoadFinished = function(status) {
+  console.log('Status: ' + status);
+
+setTimeout(function () {
+			//console.log(document.getElementById('GenerateNarrativeButton').innerHtml);//.firstChild.click();	
+  			//$("TriggerNarrative input")[0].value = "";// Math.floor(Math.random() * 1000)).toString();
+  			if(document.getElementById('Narrative') == null) {
+  				console.log("it's null");
+  			} else { 
+  				console.log("not null");
+  			}
+  			
+  			var generateNarrative = page.evaluate(function() {
+				document.getElementById('GenerateNarrativeButton').firstChild.click();
+				
+  			});
+  			
+  			
+  			console.log(generateNarrative);
+  			
+  			setTimeout(function () {
+  			
+  				var ua = page.evaluate(function() {
+      				return document.getElementById('Narrative').textContent;
+      			});
+      			console.log(ua);
+      			//page.render("sf.png");
+                phantom.exit();
+  			
+  			
+   			}, 6000);
+   			
+
+        }, 1000);
+
+
+};
